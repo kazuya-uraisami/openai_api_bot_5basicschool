fdf2feabf60788ccf30786212c0dc9392a7dd8e6
浦勇 URAISAMI 和也 Kaz <131504440+kazuya-uraisami@users.noreply.github.com>
2023-05-14 11:22:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("Your AI Partner『合格請負人』")
+st.title("Your AI Partner『合格応援くん』")
 st.write("ChatGPT APIを使ったAIパートナーです")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
